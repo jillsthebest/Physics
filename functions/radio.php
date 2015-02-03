c8ad9165d5d89e0117f1073ad1507e3846341400
jillsthebest <jill.c.russek@gmail.com>
2015-02-02 21:36:05 -0500

    "radio buttons now give a score that's animated when you click on one from a group"

--- functions/radio.php
+++ functions/radio.php
@@ -7,7 +7,7 @@
 
 /**
  * Description of radio
- *
+ * 
  * @author Jill Russek
  */
 class radio {
@@ -23,16 +23,48 @@ class radio {
        
    }
    public function buildRadio(){
-       
-       echo '<form class="jumbotron">';
+       $parent = 'accordion';//uniqid(ord($this->name));
+       //begin group of radio buttons
+       echo '<form class="panel-group bg-info" id = "'.$parent.'" >';
+       $uniqHit = uniqid("hit");
+       echo '<script> var '.$uniqHit.' = false;</script>';
+     $count = 0;
       foreach($this->array as $value  => $response){
-         echo '<input data-toggle="collapse" data-target="#'.ord($value).'" type="radio" name="'.$this->name.'" value="'.ord($value).'" >'.ucfirst($value);
-         echo '<br>';
-         echo '<div id="'.ord($value).'" class="collapse out">';
-        echo '<p>'.$response.'</p>';
-        echo '</div>';
+        $id = uniqid(ord($value));
+        //radio button
+        echo '<div class="panel panel-default bg-info">';
+        echo '<input data-parent="#'.$parent.'"  data-toggle="collapse" data-target="#'.$id.'" type="radio" name="'.$this->name.'" value="'.ord($value).'" >'.ucfirst($value);
+         //take care of score
+         echo '<script>
+             $("[value=\''.ord($value).'\']").click(function(){
+                if(!'.$uniqHit.'){
+                    '.$uniqHit.' = true;
+                    count +='.$response->score.';
+                    $("#score").html("<b>" + count + "</b>");
+                    $("#Ani'.$id.'").html("<b>\+"+'.$response->score.'+"</b>");
+                    $("#Ani'.$id.'").animate({left: \'250px\',},"slow");
+                }
+             });
+             </script>';
+         echo '</div>';
+         //response element
+         if($count == 0){
+            echo '<div id="'.$id.'" class="panel-collapse collapse">';
+ 
+         }
+         else{
+             echo '<div id="'.$id.'" class="panel-collapse collapse">';
+         }
+         //response div
+         echo '<div class="panel-body">'.$response->text;
+            //animated score
+            echo '<div style="position:absolute" id ="Ani'.$id.'">'; 
+            echo '</div>';
+         echo '</div>';
+         echo '</div>';
+         $count ++;
        }
-       //echo '</form>';
+       echo '</form>';
    }
 }
 
